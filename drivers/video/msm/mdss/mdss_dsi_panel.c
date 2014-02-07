62d5c7534d614d7a5aca306b478f679c29084771
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2014-02-07 16:25:02 +0530

    mdss: dsi: Handle gpio configuration properly
    Dsi related GPIOs are freed even when gpio request fails.
    Fix this by removing the gpio_free() calls when the gpio request
    fails. Add the gpio_request() and gpio_free()
    calls to panel reset sequence so that gpio pins would
    go to suspend state when not in use.
    
    Change-Id: Id5ef45c5cc106d2a3ced3359739037f488e1919f
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -354,18 +354,84 @@ static void mdss_dsi_panel_bklt_dcs(struct mdss_dsi_ctrl_pdata *ctrl, int level)
 }
 #endif /* QCOM Original */
 
-void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
+static int mdss_dsi_request_gpios(struct mdss_dsi_ctrl_pdata *ctrl_pdata)
+{
+	int rc = 0;
+
+#if defined(CONFIG_F_SKYDISP_EF63_SS)
+	rc = gpio_request(ctrl_pdata->octa_rst_gpio, "disp_rst_n");
+	if (rc) {
+		pr_err("request reset gpio failed, rc=%d\n", rc);
+		gpio_free(ctrl_pdata->octa_rst_gpio);
+		if (gpio_is_valid(ctrl_pdata->octa_vci_reg_en_gpio))
+			gpio_free(ctrl_pdata->octa_vci_reg_en_gpio);
+		if (gpio_is_valid(ctrl_pdata->octa_vddi_reg_en_gpio))
+			gpio_free(ctrl_pdata->octa_vddi_reg_en_gpio);
+		return -ENODEV;
+	}
+
+	rc = gpio_direction_output(ctrl_pdata->octa_rst_gpio, 1);
+	if (rc) {
+		pr_err("set_direction for lcd rst gpio failed, rc=%d\n", rc);
+		gpio_free(ctrl_pdata->octa_rst_gpio);
+		if (gpio_is_valid(ctrl_pdata->octa_vci_reg_en_gpio))
+			gpio_free(ctrl_pdata->octa_vci_reg_en_gpio);
+		if (gpio_is_valid(ctrl_pdata->octa_vddi_reg_en_gpio))
+			gpio_free(ctrl_pdata->octa_vddi_reg_en_gpio);
+		return -ENODEV;
+	}
+#else /* QCOM Original */
+	if (gpio_is_valid(ctrl_pdata->disp_en_gpio)) {
+		rc = gpio_request(ctrl_pdata->disp_en_gpio,
+						"disp_enable");
+		if (rc) {
+			pr_err("request disp_en gpio failed, rc=%d\n",
+				       rc);
+			goto disp_en_gpio_err;
+		}
+	}
+	rc = gpio_request(ctrl_pdata->rst_gpio, "disp_rst_n");
+	if (rc) {
+		pr_err("request reset gpio failed, rc=%d\n",
+			rc);
+		goto rst_gpio_err;
+	}
+	if (gpio_is_valid(ctrl_pdata->mode_gpio)) {
+		rc = gpio_request(ctrl_pdata->mode_gpio, "panel_mode");
+		if (rc) {
+			pr_err("request panel mode gpio failed,rc=%d\n",
+								rc);
+			goto mode_gpio_err;
+		}
+	}
+#endif /* QCOM Original */
+	return rc;
+
+#if !defined(CONFIG_F_SKYDISP_EF63_SS)
+mode_gpio_err:
+	gpio_free(ctrl_pdata->rst_gpio);
+rst_gpio_err:
+	if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
+		gpio_free(ctrl_pdata->disp_en_gpio);
+disp_en_gpio_err:
+	return rc;
+#endif
+}
+
+int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 {
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
 	struct mdss_panel_info *pinfo = NULL;
+	static bool gpio_request_done;
 #if defined(CONFIG_F_SKYDISP_EF63_SS)
+	int rc = 0;
 #else /* QCOM Original */
-	int i;
+	int i, rc = 0;
 #endif /* QCOM Original */
 
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
-		return;
+		return -EINVAL;
 	}
 
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
@@ -375,7 +441,7 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 	if (!gpio_is_valid(ctrl_pdata->octa_rst_gpio)) {
 		pr_debug("%s:%d, reset line not configured\n",
 			   __func__, __LINE__);
-		return;
+		return rc;
 	}
 #else /* QCOM Original */
 	if (!gpio_is_valid(ctrl_pdata->disp_en_gpio)) {
@@ -386,12 +452,21 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 	if (!gpio_is_valid(ctrl_pdata->rst_gpio)) {
 		pr_debug("%s:%d, reset line not configured\n",
 			   __func__, __LINE__);
-		return;
+		return rc;
 	}
 #endif /* QCOM Original */
 
 	pr_debug("%s: enable = %d\n", __func__, enable);
 	pinfo = &(ctrl_pdata->panel_data.panel_info);
+
+	if (!gpio_request_done && enable) {
+		rc = mdss_dsi_request_gpios(ctrl_pdata);
+		if (rc) {
+			pr_err("gpio request failed\n");
+			return rc;
+		}
+		gpio_request_done = true;
+	}
 
 	if (enable) {
 #if defined(CONFIG_F_SKYDISP_EF63_SS)
@@ -423,11 +498,18 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 #if defined(CONFIG_F_SKYDISP_EF63_SS)
 		gpio_set_value((ctrl_pdata->octa_rst_gpio), 0);
 #else /* QCOM Original */
-		gpio_set_value((ctrl_pdata->rst_gpio), 0);
-		if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
+		if (gpio_is_valid(ctrl_pdata->disp_en_gpio)) {
 			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
+			gpio_free(ctrl_pdata->disp_en_gpio);
+		}
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		gpio_free(ctrl_pdata->rst_gpio);
+		if (gpio_is_valid(ctrl_pdata->mode_gpio))
+			gpio_free(ctrl_pdata->mode_gpio);
+		gpio_request_done = false;
 #endif /* QCOM Original */
 	}
+	return rc;
 }
 
 static char caset[] = {0x2a, 0x00, 0x00, 0x03, 0x00};	/* DTYPE_DCS_LWRITE */
