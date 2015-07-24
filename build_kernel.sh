1c6e91146fe5ad8569f3d742e425e6da2a510dff
mifl <mifl.pc@gmail.com>
2015-07-24 09:44:38 +0200

    kernel: Add build/clean kernel script
    Change-Id: Iadbeb177881b4b7254b3618714c83af72df14285

---
+++ build_kernel.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+##############################################################################
+#
+# Kernel Build Script
+#
+##############################################################################
+
+##############################################################################
+# Set variables
+##############################################################################
+BUILD_KERNEL_OUT_DIR=obj
+BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo | wc -l`
+BUILD_KERNEL_LOG=kernel_log.txt
+
+##############################################################################
+# Set toolchain
+##############################################################################
+export ARCH=arm
+export PATH=$(pwd)/../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:$PATH
+export CROSS_COMPILE=arm-eabi-
+
+##############################################################################
+# Set defconfig
+##############################################################################
+DEFCONFIG_FILE=$1
+
+if [ -z "$DEFCONFIG_FILE" ]; then
+	echo "Need defconfig file(hammerhead_defconfig)!"
+	exit -1
+fi
+
+if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file : arch/arm/configs/$DEFCONFIG_FILE"
+	exit -1
+fi
+
+##############################################################################
+# Build kernel
+##############################################################################
+mkdir -p ./$BUILD_KERNEL_OUT_DIR/
+make ARCH=arm O=./$BUILD_KERNEL_OUT_DIR/ ${DEFCONFIG_FILE}
+make -j$BUILD_JOB_NUMBER ARCH=arm O=./$BUILD_KERNEL_OUT_DIR/ 2>&1 | tee $BUILD_KERNEL_LOG
+
+##############################################################################
+# Copy kernel image
+##############################################################################
+if [ -f ./$BUILD_KERNEL_OUT_DIR/arch/arm/boot/zImage ]
+then
+	cp -f ./$BUILD_KERNEL_OUT_DIR/arch/arm/boot/zImage ./
+fi
